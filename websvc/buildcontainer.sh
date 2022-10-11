a1b1b0a90e8cda1bc39a5b7f3840617bbff5058f
Dave Sudia <dsudia@datawire.io>
2022-10-11 14:34:10 -0600

    fix usersvc var in websvc build

--- websvc/buildcontainer.sh
+++ websvc/buildcontainer.sh
@@ -6,7 +6,7 @@ pack build "thedevelopnik/es-overview-websvc:$1" \
   --env "VUE_APP_CHATSVC_TCP_PROTOCOL=https://" \
   --env "VUE_APP_CHATSVC_SOCKET_PROTOCOL=wss://" \
   --env "VUE_APP_USERSVC_HOST=usersvc.thedevelopnik.com" \
-  --env "VUE_APP_CHATSVC_TCP_PROTOCOL=https://" \
+  --env "VUE_APP_USERSVC_TCP_PROTOCOL=https://" \
   --env "VUE_APP_BASE_URL=esdemo.thedevelopnik.com" \
   --builder heroku/buildpacks:20 \
   --buildpack heroku/nodejs \
