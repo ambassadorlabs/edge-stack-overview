c001a56e3c5d57f61ff3c10e47ed13752f695bb3
Dave Sudia <dsudia@datawire.io>
2022-10-11 12:21:07 -0600

    websvc fixes

--- websvc/buildcontainer.sh
+++ websvc/buildcontainer.sh
@@ -2,6 +2,12 @@
 
 pack build "thedevelopnik/es-overview-websvc:$1" \
   --creation-time now \
+  --env "VUE_APP_CHATSVC_HOST=chatsvc.thedevelopnik.com" \
+  --env "VUE_APP_CHATSVC_TCP_PROTOCOL=https://" \
+  --env "VUE_APP_CHATSVC_SOCKET_PROTOCOL=wss://" \
+  --env "VUE_APP_USERSVC_HOST=usersvc.thedevelopnik.com" \
+  --env "VUE_APP_CHATSVC_TCP_PROTOCOL=https://" \
+  --env "VUE_APP_BASE_URL=esdemo.thedevelopnik.com" \
   --builder heroku/buildpacks:20 \
   --buildpack heroku/nodejs \
   --buildpack heroku/procfile@0.6.2 \
